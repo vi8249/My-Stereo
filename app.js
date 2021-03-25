18fc2fd833bbd81b57ee071248db5261917bc763
vi8249 <vi8249@gmail.com>
2021-03-25 23:28:25 +0800

    comment out static model

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(session({
 app.use(flash());
 
 app.use(express.static("public"));
-app.use(express.static('models'));
+// app.use(express.static('models'));
 
 app.set('view engine', 'ejs');
 
